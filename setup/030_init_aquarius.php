8afa04bd4a4fe649c86db55ce405cfa9fdbb724a
Stephan Balmer <stephan@aquaverde.ch>
2012-09-05 16:02:26 +0200

    Do not load full aquarius because modules might be not available at this points.
    The DB might be empty.

--- setup/030_init_aquarius.php
+++ setup/030_init_aquarius.php
@@ -3,7 +3,6 @@
 require_once '../lib/Aquarius_Loader.php';
 $aquarius_loader = new Aquarius_loader();
 $aquarius_loader->load('aquarius');
-$aquarius_loader->load('db_connection');
 $aquarius_loader->load('globals');
 
 $cached_db_schema = $aquarius->cache_path().'schema.ini';
@@ -12,5 +11,5 @@ if (!file_exists($cached_db_schema)) {
     copy($aquarius->core_path.'lib/db/schema.ini', $cached_db_schema);
 }
 
-$aquarius_loader->load('full');
+$aquarius_loader->load('db_connection');
 
