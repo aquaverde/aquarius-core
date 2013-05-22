abe9de064d511e4c64cc67d952a5009545053edf
Stephan Balmer <stephan@aquaverde.ch>
2013-05-22 17:01:47 +0200

    Fix case where the lang_fallback module tried using inactive content

--- modules/lang_fallback/lang_fallback.php
+++ modules/lang_fallback/lang_fallback.php
@@ -1,5 +1,5 @@
 <?php
-/** Switch to another language should the desired page not be avialable in the
+/** Switch to another language should the desired page not be available in the
   * selected language.
   *
   * This is useful on sites where not all content is available in all languages.
@@ -9,7 +9,7 @@
   * there is one available. */
 class Lang_Fallback extends Module {
     var $register_hooks = array('frontend_extend_node_detection');
-    var $short = "primary_lg_fallback";
+    var $short = "lang_fallback";
     var $name  = "Switch to another language when content is missing";
 
 
@@ -25,18 +25,18 @@ class Lang_Fallback extends Module {
         $lg = $params['lg'];
         $node = $params['current_node'];
         $content = $node->get_content($lg);
-        if ($content && (!$params['require_active'] || $content->active)) {
+        if ($content && $content->active) {
             // We're good
         } else {
             // Log::warn("Suspected terrorist activity: User tried accessing deleted page $node->id in foreign language $lg. Investigate ".$_SERVER['REMOTE_ADDR']);
-            Log::debug("Page unavailable in language $lg, trying to find content in another language");
+            Log::debug("Page unavailable in language $lg, lang_fallback module trying to find content in another language");
             $all_content = $node->get_all_content();
             
             // First try to be sensitive about the user agent's configured languages, tamam mı?
             foreach(explode(",", get($params['server'], 'HTTP_ACCEPT_LANGUAGE')) as $user_language) {
                 $user_lg = substr($user_language, 0, 2);
                 foreach($all_content as $cancontent) {
-                    if ($user_lg == $cancontent->lg) {
+                    if ($cancontent->active && $user_lg == $cancontent->lg) {
                         /* We do actually have content in one of the languages
                          * that the user agent says it accepts. REJOICE! CELEBRATE! REDIRECT! */
                         return array('lg' => $cancontent->lg);
