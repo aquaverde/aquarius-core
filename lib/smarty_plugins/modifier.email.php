81f12e5a92e622095a9fe01f68f2abd856b88f0a
ThomasSchmid <schmed@recondite.ch>
2013-11-29 16:26:31 +0100

    new email modifier

--- lib/smarty_plugins/modifier.email.php
+++ lib/smarty_plugins/modifier.email.php
@@ -1,12 +1,20 @@
 <?php
-/** Format E-Mail with Javascript (anti spam war) */
+/*  Format E-Mail with Javascript (anti spam war) 
+    
+    standard use:
+    <p>{$email|email}</p>
+    
+    use with own linktext:
+    {capture assign=linktext}{wording E-Mail Adress}{/capture}
+    <p>{$email|email:$linktext}</p>
+*/
 
-function smarty_modifier_email($email)
+function smarty_modifier_email($email, $linktext)
 {
 	list($name,$domainTLD)	= split("@", $email);
 	$domain	= substr($domainTLD, 0, strrpos($domainTLD, "."));
 	$tld	= substr($domainTLD, strrpos($domainTLD, ".") + 1, strlen($domainTLD));
 	
-	return '<script type="text/javascript">email(\''.$name.'\', \''.$domain.'\', \''.$tld.'\');</script>';
+	return '<span class="shield" data-local="'.$name.'" data-domain="'.$domain.'" data-tld="'.$tld.'" data-linktext="'.$linktext.'"></span>';
 }
 ?>
