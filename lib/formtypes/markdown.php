819a14bc4750e0ba1354fb243103716e219cb0d2
julienchriste <julien@aquaverde.ch>
2014-01-21 16:03:32 +0100

    add formtype "markdown"

---
+++ lib/formtypes/markdown.php
@@ -0,0 +1,9 @@
+<?php 
+class Formtype_Markdown extends Formtype {
+    /** Apply formtype specific conversion prior to editing content
+     */
+    function pre_contentedit($node, $content, $formtype, $formfield, $valobject) {
+        $valobject->rows = max(3, $formfield->sup1); // Make it at least three lines
+    }
+}
+?>
---
+++ templates/formfield.markdown.tpl
@@ -0,0 +1,2 @@
+<span class="label label-default pull-right">Markdown</span>
+<textarea class="form-control code markdown" name="{$field.formname}" rows="{$field.rows}" cols="80" id="{$field.htmlid}">{$field.value|escape}</textarea>
