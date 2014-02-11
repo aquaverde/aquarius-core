61e6b366b2a5402ce098ace969e0e7869441cb10
Stephan Balmer <stephan@aquaverde.ch>
2014-02-11 14:33:07 +0100

    Add some logging to evaling legacy PHP tags in smarty

--- lib/Aqua_Smarty.php
+++ lib/Aqua_Smarty.php
@@ -4,6 +4,16 @@ class Aqua_Smarty extends SmartyBC {
     var $require_active;
     var $uri; /* link factory */
     
+
+    public function __construct(array $options=array()) {
+        
+        parent::__construct($options);
+        
+        // Override handler for the {php} tag
+        $this->unregisterPlugin('block', 'php');
+        $this->registerPlugin('block', 'php', array($this, 'eval_php'));
+    }
+
     /** Insert template dir in front of other template dirs
       * Otherwise same as addTemplateDir()
       */
@@ -23,6 +33,10 @@ class Aqua_Smarty extends SmartyBC {
     }
     
     function fetch($template = null, $cache_id = null, $compile_id = null, $parent = null, $display = false, $merge_tpl_vars = true, $no_output_filter = false) {
+        // Override handler for the {php} tag
+        $this->unregisterPlugin('block', 'php');
+        $this->registerPlugin('block', 'php', array($this, 'eval_php'));
+        
         set_error_handler(array($this, 'receive_error'), error_reporting());
         $result = parent::fetch($template, $cache_id, $compile_id, $parent, $display, $merge_tpl_vars, $no_output_filter);
         restore_error_handler();
@@ -32,5 +46,17 @@ class Aqua_Smarty extends SmartyBC {
     function receive_error($errno, $errstr, $errfile, $errline, $errcontext) {
         Log::debug(compact('errno', 'errstr', 'errfile', 'errline'));
         return true;
+    }
+
+    /** Custom error logging over the use of the {php} tag */
+    function eval_php($params, $content, $template, &$repeat) {
+        $result = eval($content);
+        if ($result === false) {
+            $error = error_get_last();
+            $error['template'] = $template->template_resource;
+            $error['code'] = $content;
+            Log::debug($error);
+        }
+        return '';
     }
 }
