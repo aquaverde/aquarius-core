bca7d987d8d2caaee918c9c61129ff3a9844ef9c
Stephan Balmer <stephan@aquaverde.ch>
2014-08-19 14:30:43 +0200

    Update may_edit() so it can deal with newly inserted nodes as well
    In these cases the tree index has not been rebuilt yet, so we must use the parent's position in the tree.

--- lib/db/Users.php
+++ lib/db/Users.php
@@ -285,17 +285,22 @@ class db_Users extends DB_DataObject
 
         // Users may edit a node if they have permission to edit that node or one of its parents
         if (!isset($this->_cache_edit_ranges)) {
-            global $DB;
-            $this->_cache_edit_ranges = $DB->mapqueryhash('node_id', '
+            global $aquarius;
+            $this->_cache_edit_ranges = $aquarius->db->mapqueryhash('node_id', '
                 SELECT node.id AS node_id, node.cache_left_index AS left_index, node.cache_right_index AS right_index
                 FROM node
                 JOIN users2nodes ON node.id = users2nodes.nodeId
-                WHERE users2nodes.userId = '.$this->id.'
-            ');
+                WHERE users2nodes.userId = ?', 
+                $this->id);
         }
+        
+        // Check whether node is in a permitted edit _cache_edit_range
+        // In case of new nodes, the tree index is not available, so we check against its parent
+        $check_against = $node;
+        if (!$node->cache_right_index) $check_against = $node->get_parent();
         foreach($this->_cache_edit_ranges as $permitted_range) {
-            if ( $permitted_range['left_index'] <= $node->cache_left_index
-            && $permitted_range['right_index'] >= $node->cache_right_index
+            if ( $permitted_range['left_index'] <= $check_against->cache_left_index
+            && $permitted_range['right_index'] >= $check_against->cache_right_index
             ) return true;
         }
         return false;
