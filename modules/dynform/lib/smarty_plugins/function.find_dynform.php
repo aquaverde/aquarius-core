bd06a0cf710be51e455e24a474241394bc41c85e
Stephan Balmer <stephan@aquaverde.ch>
2014-09-13 18:49:35 +0200

    convenience function to find the dynform node

---
+++ modules/dynform/lib/smarty_plugins/function.find_dynform.php
@@ -0,0 +1,22 @@
+<?php 
+/** Check the nodes given as parameters. 
+  * The first node that has a dynform available will be assigned to the dynform variable.
+  */
+function smarty_function_find_dynform($params, $smarty) {
+    foreach($params as $param) {
+        $node = db_Node::get_node($param);
+        if ($node) {
+            require_once 'lib/libdynform.php';
+
+            $DL = new Dynformlib;
+            
+            $dynform = new db_Dynform ; 
+            $dynform->node_id = $form_node->id ;
+            $dynform_available = $dynform->find() ; 
+            if ($dynform_available) {
+                $smarty->assign("dynform", $node) ;
+                break;
+            }
+        }
+    }
+}
