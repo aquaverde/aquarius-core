861906fe3cdd51b40df5e6de0ade57ae8a47bde2
Stephan Balmer <stephan@aquaverde.ch>
2015-02-10 14:44:17 +0100

    modernize push helper a bit
    the developement speed of this one is only surpassed by glaciers

--- lib/smarty_frontend_plugins/block.pushed.php
+++ lib/smarty_frontend_plugins/block.pushed.php
@@ -1,8 +1,8 @@
 <?php 
 /** List pushed childs of some node.
   * Params:
-  *   childsof: Look for pushed content in the descendants of this node
-  *   field: Field that must be set (default: push)
+  *   childrenof: Restrict to descendants of this node (DEPRECATED name childsof supported as well)
+  *   field: Field that must be set (preset: push)
   *   sort: sort by content_field
   *   limit: Maximum count of elements to show
   *   reverse: reverse sorting order (applied after limiting result)
@@ -16,15 +16,11 @@ function smarty_block_pushed($params, $content, $smarty, &$repeat) {
     static $list;
     // On first invocation get elements for list
     if ($repeat) {
-        $node = db_Node::get_node(get($params, 'childsof'));
-        if (!$node) throw new Exception("Childsof node invalid, got ".get($params, 'childsof'));
-        $limit = intval(get($params, 'limit'));
-        $pushed = mysql_real_escape_string(get($params, 'field', 'push'));
-        $sort = mysql_real_escape_string(get($params, 'sort', false));
-        $lg = mysql_real_escape_string($smarty->get_template_vars('lg'));
+        $ancestor = db_Node::get_node(get($params, 'childrenof', get($params, 'childsof')));
 
         // Don't we all like complex SQL? I sure do :-) (though it turned ugly with the content_field_value table)
         // In essence, we're looking for content that has a field $pushed = 1, and we want to retrieve the content ordered after another content field. Thus we have to join in the content_field (and content_field_value) table twice, first for detecting a pushed content (aliased field_pushed) and second to order by the $sort field (aliased field_order).
+        $data = array();
         $query = "
             SELECT DISTINCT content.id"; 
         if ($sort) $query .= ", field_order_value.value as order_value " ;
@@ -36,29 +32,54 @@ function smarty_block_pushed($params, $content, $smarty, &$repeat) {
               JOIN content_field_value field_pushed_value ON field_pushed_value.content_field_id = field_pushed.id";
         if ($sort) $query .= "
               JOIN content_field field_order ON field_order.content_id = content.id
-              JOIN content_field_value field_order_value ON field_order_value.content_field_id = field_order.id" ;      
-        $query .= "      
-            WHERE
-                  node.cache_left_index > '$node->cache_left_index'
-              AND node.cache_right_index < '$node->cache_right_index'
-              AND content.lg = '$lg'
-              AND field_pushed.name = '$pushed'
-              AND field_pushed_value.value = 1" ;
-        if ($sort) $query .= "
-              AND field_order.name = '$sort'" ;
+              JOIN content_field_value field_order_value ON field_order_value.content_field_id = field_order.id
+        ";
+
+        $query .= "
+            WHERE 1=1";
+        
+        if ($ancestor) {
+            $query .= "
+              AND node.cache_left_index > ?
+              AND node.cache_right_index < ?
+            ";
+            $data []= $ancestor->cache_left_index;
+            $data []= $ancestor->cache_right_index;
+        }
+        $query .= "
+              AND content.lg = ?
+              AND field_pushed.name = ?
+              AND field_pushed_value.value = 1
+        ";
+        $data []= $smarty->get_template_vars('lg');
+        $data []= get($params, 'field', 'push');
+
+        $sort = get($params, 'sort', false);
+        if ($sort) {
+            $query .= "
+              AND field_order.name = ?
+            ";
+            $data []= $sort;
+        }
+
         $query .= "
               AND node.active = 1
               AND content.active = 1
-            ORDER BY " ;
+            ORDER BY 
+        ";
 
         if ($sort)                       $query .= 'order_value' ;
         elseif (get($params,'shuffle'))  $query .= 'RAND()' ;
-        else                             $query .= 'node.weight' ;            
+        else                             $query .= 'node.weight' ;
 
-        if ($limit > 0) $query .= "
-            LIMIT 0,$limit";
+        $limit = intval(get($params, 'limit'));
+        if ($limit > 0) {
+            $query .= "LIMIT 0,?";
+            $data []= $limit;
+        }
 
-        $list = $GLOBALS['DB']->listquery($query);
+        global $aquarius;
+        $list = $aquarius->db->listquery($query, $data);
         if (get($params, 'reverse')) $list = array_reverse($list);
     }
 
