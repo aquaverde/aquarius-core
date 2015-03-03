b9a3e1bc54c7cb0a8277ebb207a504cebab9d60f
Stephan Balmer <stephan@aquaverde.ch>
2015-03-03 16:35:51 +0100

    show more detailed form usage counts
    link to an example for each count

--- lib/action/formlist.php
+++ lib/action/formlist.php
@@ -11,7 +11,45 @@ class action_formlist extends AdminAction implements DisplayAction {
     function process($aquarius, $request, $smarty, $result) {
         require_once "lib/db/Form.php";
 
-        $smarty->assign('forms', db_Form::get_all());
+        $formcts = array();
+        foreach($aquarius->db->queryhash('
+            SELECT
+                form.id form_id,
+                count(applied.id) as applied_count,
+                min(applied.id) as applied_example,
+                count(chosenchild.id) + count(chosencont.id) as nodechild_count,
+                min(chosenchild.id) as nodechild_example, 
+                min(chosencont.id) as nodecont_example,
+                count(chosenform.id) as formchild_count,
+                min(chosenform.parent_id) as formchild_example
+            FROM form
+            LEFT JOIN node applied ON form.id = applied.form_id
+            LEFT JOIN node chosenchild ON form.id = chosenchild.childform_id
+            LEFT JOIN node chosencont ON form.id = chosencont.contentform_id
+            LEFT JOIN form_child chosenform ON form.id = chosenform.child_id
+            GROUP BY form_id;
+        ') as $formdesc) {
+            $form = DB_DataObject::factory('form');
+            $form->get($formdesc['form_id']);
+            
+            $nodechild_example = min(intval($formdesc['nodechild_example']), intval($formdesc['nodecont_example']));
+            
+            $formcts []= array(
+                'form' => $form,
+                'count' => array(
+                    'node' => $formdesc['applied_count'],
+                    'nodechild' => $formdesc['nodechild_count'],
+                    'formchild' => $formdesc['formchild_count'],
+                ),
+                'example' => array(
+                    'node' => $formdesc['applied_example'] ? Action::make('node', 'editprop', $formdesc['applied_example']) : false,
+                    'nodechild' => $nodechild_example ? Action::make('node', 'editprop', $nodechild_example) : false,
+                    'formchild' => $formdesc['formchild_example'] ? Action::make('formedit', 'edit', $formdesc['formchild_example']) : false
+                ),
+            );
+        }
+
+        $smarty->assign('formcts', $formcts);
         $smarty->assign('action_new', Action::make('formedit', 'edit', 'new'));
         $result->use_template("formlist.tpl");
     }
