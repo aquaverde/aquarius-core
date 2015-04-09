cf8665241c2b7ca05b0a6cf13a478ff82c1357c9
Stephan Balmer <stephan@aquaverde.ch>
2015-04-09 12:49:58 +0200

    modernise tableexport

--- modules/tableexport/lib/action/tableexport.php
+++ modules/tableexport/lib/action/tableexport.php
@@ -24,26 +24,17 @@ class action_tableexport extends ModuleAction {
             return null;
         } 
     }
-    
-    function execute() {
-        global $aquarius;
-        
-        require_once "lib/db/Node.php";
-        
-        $smarty = false;
-        $messages = array();
-        $action = false;
-        
-        switch($this->op) {
-        case 'view1':
-        case 'view':
-            $smarty = $aquarius->get_smarty_backend_container();
-            
+}
+
+
+class Action_Tableexport_view extends action_tableexport implements DisplayAction {
+    var $props = array('class', 'op');
+    function process($aquarius, $get, $smarty, $result) {
             $bookings_proto = DB_DataObject::factory(TABLEEXPORT_TABLE);
 
             $bookings_proto->orderBy(TABLEEXPORT_ORDERCOLUMN);
             $bookings_proto->find();
-            
+
             $columns = array_keys($bookings_proto->table());
             $keyfields = $bookings_proto->keys();
             $keyfield = $keyfields[0];
@@ -56,14 +47,15 @@ class action_tableexport extends ModuleAction {
             $smarty->assign('bookings_proto', $bookings_proto);
             $smarty->assign('bookings', $bookings);
             $smarty->assign('keyfield', $keyfield);
-            $smarty->tmplname = "tableexport_view.tpl";
-            break;
-        
-        case 'edit':
+            $result->use_template("tableexport_view.tpl");
+    }
+}
+class Action_Tableexport_view1 extends action_tableexport_view implements DisplayAction {} // It was already like that when I got it
+
+class Action_Tableexport_edit extends action_tableexport implements ChangeAction {
+    function process($aquarius, $post, $result) {
             global $admin_lg;
             if($this->id != 'null') {
-                
-                $smarty = $aquarius->get_smarty_backend_container();
                 $bookings_proto = DB_DataObject::factory(TABLEEXPORT_TABLE);
                 $keyfields = $bookings_proto->keys();
                 $keyfield = $keyfields[0];
@@ -84,13 +76,16 @@ class action_tableexport extends ModuleAction {
                 if ($form->validate()) {
                     $form->process(array(&$fg,'processForm'), false);
                     $form->freeze();
-                    $smarty = null;
-                    $action = Action::make('tableexport','view','null');
-                    $messages[] = new Translation("tableexport_entry_edited");
+                    $action = Action::make('tableexport','view');
+                    $result->add_message(new Translation("tableexport_entry_edited"));
                 }
             }
-            break;
-        case 'delete':
+    }
+}
+
+
+class Action_Tableexport_delete extends action_tableexport implements ChangeAction {
+    function process($aquarius, $post, $result) {
             if($this->id != 'null') {
                 $bookings_proto = DB_DataObject::factory(TABLEEXPORT_TABLE);
                 $key = $bookings_proto->keys();
@@ -98,64 +93,58 @@ class action_tableexport extends ModuleAction {
                 $bookings_proto->find();
                 $bookings_proto->delete();
             }
-            $messages[] = new WordingTranslation("tableexport_entry_deleted");
-            break;
-        case 'export':
+            $result->add_message(new Translation("tableexport_entry_deleted"));
+    }
+}
 
-            $smarty = $aquarius->get_smarty_backend_container();
-            $smarty->tmplname = "tableexport_export.tpl";
-            break;
-        case 'exportdown':
-            ob_clean();
-            header('Content-type: text/csv');
-            header('Content-Disposition: attachment; filename="'.TABLEEXPORT_FILENAME.'"');
+
+class Action_Tableexport_deleteallconfirm extends action_tableexport implements DisplayAction {
+    function process($aquarius, $get, $smarty, $result) {
             $bookings = DB_DataObject::factory(TABLEEXPORT_TABLE);
             $count = $bookings->find();
-            $bookingsarray = array();
-            while($bookings->fetch()) {
-                $bookingsarray[] = $bookings->toArray();
-            }
-            array_walk_recursive($bookingsarray, create_function(
-                '&$item,$key', '$item = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $item);'));
-
-            $out = fopen("php://output", "w");
-          
-            $columns = array_keys($bookings->table());
-             if(!function_exists("fputcsv")) {
-                aqua_fputcsv($out, $columns,';','"');
-            } else {
-                fputcsv($out, $columns,';','"');
-            }
-            if($count > 0) {
-                foreach($bookingsarray as $line) {
-                    if(!function_exists("fputcsv")) {
-                        aqua_fputcsv($out, $line,';','"');
-                    } else {
-                        fputcsv($out, $line,';','"');
-                    }
-                }
-            }
-            exit();
-            break;
-        case 'deleteallconfirm':
-            $bookings = DB_DataObject::factory(TABLEEXPORT_TABLE);
-            $count = $bookings->find();
-            $smarty = $aquarius->get_smarty_backend_container();
             $smarty->assign('count', $count);
             $smarty->assign('deleted', false);
-            $smarty->tmplname = "tableexport_delete.tpl";
-            break;
-        case 'deleteall':
+            $result->use_template("tableexport_delete.tpl");
+    }
+}
+
+
+class Action_Tableexport_deleteall extends action_tableexport implements ChangeAction {
+    function process($aquarius, $post, $result) {
             $bookings = DB_DataObject::factory(TABLEEXPORT_TABLE);
             $bookings->find();
             while($bookings->fetch()) {
                 $bookings->delete();
             }
-            break;
-        default:
-            throw new Exception("Operation unknown: '$this->op'");
-        }
-        return compact('messages', 'smarty', 'action');
     }
 }
+
+
+class Action_Tableexport_export extends action_tableexport implements DisplayAction {
+    var $props = array('class', 'op');
+    function process($aquarius, $get, $smarty, $result) {
+        $result->use_template("tableexport_export.tpl");
+    }
+}
+
+
+class Action_Tableexport_exportdown extends action_tableexport implements SideAction {
+    var $props = array('class', 'op');
+    function process($aquarius, $get) {
+            ob_clean();
+            header('Content-type: text/csv; charset=utf-8');
+            header('Content-Disposition: attachment; filename="'.TABLEEXPORT_FILENAME.'"');
+            $bookings = DB_DataObject::factory(TABLEEXPORT_TABLE);
+
+            $out = fopen("php://output", "w");
+
+            $columns = array_keys($bookings->table());
+            aqua_fputcsv($out, $columns);
+
+            while($bookings->fetch()) {
+                aqua_fputcsv($out, $bookings->toArray());
+            }
+    }
+}
+
 
