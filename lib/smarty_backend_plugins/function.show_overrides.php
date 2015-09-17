9b849c1f80851a249bc30263b77513e4a7fd16f5
Stephan Balmer <stephan@aquaverde.ch>
2015-09-17 14:51:34 +0200

    explain what hte overrides do

--- lib/smarty_backend_plugins/function.show_overrides.php
+++ lib/smarty_backend_plugins/function.show_overrides.php
@@ -1,11 +1,15 @@
 <?php
 
 function smarty_function_show_overrides() {
-
+    $overrides = array(
+        'DEBUG'   => "Directly logging to content, view source to see it.",
+        'DEV'     => "Login with any username no password, caching disabled, and domain configuration ignored.",
+        'STAGING' => "Domain configuration ignored"
+    );
     echo "<ul style='display: inline;'>";
-    foreach(array('DEBUG', 'DEV', 'STAGING') as $name) {
+    foreach($overrides as $name => $explains) {
         if (constant($name)) {
-            echo "<li style='display: inline; margin: 0.5em; padding: 0.5em; background-color: green' title='".join(', ', $overrides)."'>$name</li>";
+            echo "<li style='display: inline; margin: 0.5em; padding: 0.5em; background-color: green' title='$explains'>$name</li>";
         }
     }
     echo "</ul>";
