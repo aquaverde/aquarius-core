5f84e4f505917a800bda0f97955dfdbedb77fffd
Stephan Balmer <stephan@aquaverde.ch>
2016-01-12 19:21:09 +0100

    PHP7 fixes

--- lib/db/Users2nodes.php
+++ lib/db/Users2nodes.php
@@ -11,10 +11,7 @@ class db_Users2nodes extends DB_DataObject
     public $__table = 'users2nodes';                     // table name
     public $userId;                          // int(11)  not_null primary_key group_by
     public $nodeId;                          // int(11)  not_null primary_key group_by
-
-    /* Static get */
-    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('db_Users2nodes',$k,$v); }
-
+   
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
 
