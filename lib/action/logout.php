54b2ee297868dc191bf25228867d68cbf4e09cc2
Stephan Balmer <sb@cis.ch>
2018-06-23 02:46:57 +0200

    remove logout redirect hack
    since we do post->redirect->get after ChangeAction, it's not needed
    anymore.

--- lib/action/logout.php
+++ lib/action/logout.php
@@ -12,13 +12,6 @@ class action_logout extends AdminAction {
 class action_logout_now extends action_logout implements ChangeAction {
     function process($aquarius, $post, $result) {
       db_Users::logout();
-      $result->inject_action(Action::make('logout', 'redirect'));
     }
 }
 
-/** Redirect to the login screen */
-class action_logout_redirect extends action_logout implements DisplayAction {
-    function process($aquarius, $request, $smarty, $result) {
-      $result->use_template("login-redirect.tpl");
-    }
-}
